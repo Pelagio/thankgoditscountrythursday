d99ecb074f4f12fa6b2ed16544ee0cf11ae28559
Simon Takman <simon.takman@pelag.io>
2023-06-08 10:07:23 +0200

    Update w23

--- src/App.tsx
+++ src/App.tsx
@@ -10,48 +10,46 @@ function App() {
   return isThursday ? (
     <div className="container">
       <img className="logo" src={CountryThursday} />
-      {/* <h1>Thinking 'Bout You (feat. MacKenzie)- Dustin Lynch</h1> */}
-      <SpotifyTrack trackId="4wBjKhJx6JnGoNJfJs24pd" />
+      <SpotifyTrack trackId="2kYevr8cbIAH2ha8210Bnz" />
       <h2>Review</h2>
       <p>
-        Dustin Lynch's collaboration with MacKenzie in the track "Thinking 'Bout
-        You" is a refreshing addition to the country music scene. With its
-        heartfelt lyrics and catchy melodies, this song showcases Lynch's growth
-        as an artist and his ability to create an emotional connection with his
-        audience.
+        Luke Combs' "Love You Anyway" is a heartfelt country ballad that
+        showcases Combs' exceptional talent as a singer-songwriter. The song
+        delves into the complexities of a relationship and the willingness to
+        stand by someone through thick and thin. Combs' rich and soulful voice
+        brings an emotional depth to the lyrics, making it a truly captivating
+        listening experience.
       </p>
       <p>
-        The chemistry between Lynch and MacKenzie is evident from the first
-        note. Their voices blend effortlessly, creating a harmonious duet that
-        is both captivating and soul-stirring. The raw emotions conveyed in
-        their delivery make it easy for listeners to relate to the longing and
-        nostalgia expressed in the lyrics.
+        The song opens with delicate acoustic guitar strums, creating an
+        intimate atmosphere that perfectly complements Combs' vulnerable vocals.
+        As the track progresses, it gradually builds with the addition of subtle
+        drums, melodic piano, and soaring strings. The production is tastefully
+        done, allowing Combs' voice to take center stage while providing a solid
+        foundation for the song.
       </p>
       <p>
-        The production of "Thinking 'Bout You" is polished and well-crafted,
-        with a perfect balance between traditional country elements and modern
-        sensibilities. The instrumentation, including the twangy guitars and
-        subtle use of synths, adds depth to the song without overpowering the
-        vocals.
+        Lyrically, "Love You Anyway" shines with its honest and relatable
+        storytelling. Combs' poignant lyrics paint a vivid picture of the
+        struggles and triumphs of a committed relationship. The chorus is
+        particularly memorable, with its infectious melody and heartfelt
+        declaration of unwavering love.
       </p>
       <p>
-        Lyrically, the song explores the bittersweet experience of reminiscing
-        about a past love. The storytelling is honest and evocative, painting a
-        vivid picture of the protagonist grappling with the memories that haunt
-        them. The chorus, with its infectious melody, is an instant earworm that
-        lingers in your mind long after the song has ended.
+        Luke Combs' delivery is nothing short of exceptional, effortlessly
+        conveying the raw emotions embedded in the song. His ability to connect
+        with listeners on a personal level is undeniable, making "Love You
+        Anyway" a standout track in his discography.
       </p>
       <p>
-        "Thinking 'Bout You" is a standout track that showcases Dustin Lynch's
-        artistic growth and his ability to deliver an emotionally resonant
-        performance. With its captivating vocals, compelling lyrics, and
-        memorable melodies, this collaboration is a testament to the enduring
-        power of country music.
+        Overall, "Love You Anyway" is a beautifully crafted country ballad that
+        showcases Luke Combs' incredible talent and songwriting prowess. It's a
+        must-listen for fans of heartfelt, soul-stirring music.
       </p>
       <h2>Playlist of the week</h2>
       <p>Based on this week's review, we have populated a playlist for you.</p>
       <p>Enjoy!</p>
-      <SpotifyPlaylist playListId="37i9dQZF1E8NJ812DhgupL" darkTheme compact />
+      <SpotifyPlaylist playListId="37i9dQZF1E8MZA2jnDjFP5" darkTheme compact />
     </div>
   ) : (
     <div className="container">
